d4b0a00ff7eab2e61fe60680afdcf9d43bfe2961
Federico Piedrasanta <fede.piedrasanta@gmail.com>
2022-05-15 16:05:52 -0300

    agregué método submit

---
+++ mvc_project/Controllers/ComponentUse/SubmitMethodController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using mvc_project.Models.Common;
+using mvc_project.Models.ComponentUse;
+
+namespace mvc_project.Controllers
+{
+    public class SubmitMethodController : Controller
+    {
+        private readonly ILogger<SubmitMethodController> _logger;
+
+        public SubmitMethodController(ILogger<SubmitMethodController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(int id)
+        {
+            TestSubmitModel testSubmitModel = null;
+
+            if(id != 0)
+            {
+                testSubmitModel = new TestSubmitModel
+                {
+                    id = id,
+                    name = "Juan",
+                    surname = "Perez"
+                };
+            }
+            else
+            {
+                testSubmitModel = new TestSubmitModel
+                {
+                    id = 0,
+                    name = "",
+                    surname = ""
+                };
+            }
+
+            return View(testSubmitModel);
+        }
+
+        public IActionResult Submit(TestSubmitModel testSubmitModel)
+        {
+            //Guardo en la BD
+            
+            return Redirect("~/Home/Index");
+        }
+    }
+}
---
+++ mvc_project/Models/ComponentUse/TestSubmitModel.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace mvc_project.Models.ComponentUse
+{
+    public class TestSubmitModel
+    {
+        public int id { get; set; }
+        
+        public string name { get; set; }
+
+        public string surname { get; set; }
+    }
+}
