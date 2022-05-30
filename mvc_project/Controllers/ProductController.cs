1406ce34ca19815e984b3805593ddd817f7647ef
Diego Martin <diegomartin00072@gmail.com>
2022-05-30 11:51:00 -0300

    EnlaceProductosyLista

---
+++ mvc_project/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace mvc_project.Controllers
+{
+    public class ProductController : Controller
+    {
+      
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        
+
+        // GET: ProductController/Edit/5
+        public ActionResult Update(int id)
+        {
+            return View();
+        }
+        public ActionResult Delete(int id)
+        {
+            return Redirect("~/Panel/Dashboard");
+        }
+
+
+    }
+}
---
+++ mvc_project/Models/Entities/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace mvc_project.Models.Entities
+{
+    public class Product
+    {
+        public int IdProduct { get; set; }
+        public string NameProduct { get; set; }
+        public float Price { get; set; }
+        public string Description { get; set; }
+    }
+}
